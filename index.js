db7e1fe9acfbb0a3d6fa5b1591a9de61101cc3ae
Maros Karasek <maros.karasek@gmail.com>
2023-12-05 15:43:46 +0100

    changes

--- index.js
+++ index.js
@@ -2,31 +2,53 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+// your first API endpoint...
+app.get('/api/hello', function(req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(3001, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get('/api/:date?', (req,res) => {
+  console.log(req.params['date'])
+  function isValidDate(d) {
+    return d instanceof Date && !isNaN(d);
+  }
+
+  let dateParam = req.params['date'];
+  let date = new Date();
+
+  if(dateParam !== undefined) {
+    let num = Number(dateParam);
+    if (!isNaN(num)) {
+      dateParam = +dateParam;
+    }
+
+    date = new Date(dateParam);
+  }
+
+  if(!isValidDate(date)) {
+    res.json({ error : "Invalid Date"})
+  }
+
+  res.json({unix: Math.floor(date.getTime()), utc: date.toUTCString()})
+})
